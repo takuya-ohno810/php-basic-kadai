163d2eb1ef4331bc46456b87821d2e13f73614c0
takuya-ohno810 <sunlegend.spike.seiha@gmail.com>
2024-11-16 13:59:11 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編_課題007</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      //連想配列に値を代入する
+      $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      //連想配列を出力する
+      print_r($onion_data);
+      ?>
+    </p>
+  </body>
+</html>
